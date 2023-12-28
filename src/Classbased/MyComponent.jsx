f5d9b5e3593cc77ea5025f959fc4c931f9166d9c
Mayank Kumar <mayank.kumar@oodles.io>
2023-12-28 17:23:21 +0530

    Mounting phase

--- src/Classbased/MyComponent.jsx
+++ src/Classbased/MyComponent.jsx
@@ -8,15 +8,40 @@ class MyComponent extends Component{
         this.state={
             name:"Mayank",
             age:"21",
+            derivedday:null,
         }
+        console.log("Constructor call");
+    }
+
+    static getDerivedStateFromProps(nextProps,prevState){
+                
+         console.log("static getDerivedStateFromProps call");
+        //  console.log(nextProps.day);
+        //  console.log(prevState.derivedday);
+        if(nextProps.day !== prevState.derivedday){
+
+            return{
+
+                derivedday: nextProps.day ,
+                
+            }; 
+        }
+
+            return null;
+
+    }
+
+    componentDidMount(){
+        console.log("component did mount call");
     }
     
     render(){
+        console.log("render method call");
         return <>
         <h1>Here name  and age is visible due to state:</h1>
         <p> Name -{this.state.name} , Age -{this.state.age} </p> 
-        /* by using props we pass data from App to Mycomponent */
         <p>Day : {this.props.day}</p>
+        <p>derivedday value from props - {this.state.derivedday}</p>
         </>;
     }
 
